a0f29ae0a926acadf8fecc27cdfc261b3862174c
Tom Lynch <me@unknowndomain.co.uk>
2016-03-23 11:57:33 +0000

    Changes to Docker setup script

--- scripts/run.sh
+++ scripts/run.sh
@@ -2,13 +2,13 @@
 
 # This will need to be run in the root of repo: docker build -t lcc-checkout . 
 
-docker run --name mongodb -v /Users/Staff/Sites/checkout:/data -d mongo:latest
+docker run --name mongo -v /Users/Staff/Sites/checkout:/data -d mongo:latest
 
 sleep 10;
 
 docker run -d \
     --name checkout \
-    --link mongodb:mongo \
+    --link mongo:mongo \
     -p 80:80 \
     -e ME_CONFIG_OPTIONS_EDITORTHEME="ambiance" \
     -e ME_CONFIG_BASICAUTH_USERNAME="" \
