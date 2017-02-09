322959729ec5412f926098714870d86d54f68bbc
unknowndomain <me@unknowndomain.co.uk>
2017-02-09 05:08:11 +0000

    Clipboard copy of barcode

--- static/js/script.js
+++ static/js/script.js
@@ -1,1 +1,11 @@
 var socket = io();
+
+jQuery( document ).ready( function() {
+	var clipboard = new Clipboard( '.clipboard' );
+	clipboard.on( 'success', function( e ) {
+		console.log( e );
+	} );
+	clipboard.on( 'error', function( e ) {
+		console.log( e );
+	} );
+} );
