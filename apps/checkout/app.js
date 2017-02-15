2945b4c87eed83049e7327a7d21bae259bab24d3
unknowndomain <me@unknowndomain.co.uk>
2017-02-15 18:18:33 +0000

    Checkout app working

--- apps/checkout/app.js
+++ apps/checkout/app.js
@@ -1,10 +1,20 @@
+var __home = __dirname + "/../..";
+var __src = __home + '/src';
+var __js = __src + '/js';
+
 var	express = require( 'express' ),
 	app = express();
 
+var auth = require( __js + '/authentication' );
+
 app.set( 'views', __dirname + '/views' );
 
-app.get( '/', function ( req, res ) {
-	res.sendStatus( 200 );
+app.get( '/', auth.isLoggedIn, function ( req, res ) {
+	res.render( 'checkout' );
+} );
+
+app.get( '/compact', auth.isLoggedIn, function( req, res ) {
+	res.render( prefix + '/compact-checkout' );
 } );
 
 module.exports = function( config ) { return app; };
