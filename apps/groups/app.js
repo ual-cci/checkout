d82be9736f957dcd454d3070a5cd1c36e3c02e21
Tom Lynch <me@unknowndomain.co.uk>
2018-12-17 22:24:32 +0000

    Add logging to groups as an example for @jthawme

--- apps/groups/app.js
+++ apps/groups/app.js
@@ -27,21 +27,44 @@ app.post( '/create', auth.isLoggedIn, function( req, res ) {
 	if ( req.body.name == '' ) {
 		req.flash( 'danger', 'The group requires a name' );
 		res.redirect( app.mountpath + '/create' );
+
+		req.log.debug( {
+			app: 'groups',
+			action: 'create',
+			error: 'group-name-missing',
+		} );
 	}
 
 	var group = {
 		name: req.body.name
 	}
 
-  if ( req.body.limiter ) group.limiter = req.body.limiter;
+	if ( req.body.limiter ) group.limiter = req.body.limiter;
 
 	Groups.create( group, function( err, group ) {
 		if ( err ) {
 			req.flash( 'danger', `Error creating group: ${err.detail}` );
 			res.redirect( app.mountpath );
+
+			req.log.warn( {
+				app: 'groups',
+				action: 'create',
+				error: 'error-creating-group',
+				sensitive: {
+					error: err
+				}
+			} );
 		} else {
 			req.flash( 'success', 'Group created' );
 			res.redirect( app.mountpath );
+
+			req.log.debug( {
+				app: 'groups',
+				action: 'create',
+				sensitive: {
+					group: group
+				}
+			} );
 		}
 	} );
 } )
@@ -51,6 +74,12 @@ app.get( '/:id/edit', auth.isLoggedIn, function( req, res ) {
 		if ( group == undefined ) {
 			req.flash( 'danger', 'Group not found' );
 			res.redirect( app.mountpath );
+
+			req.log.warn( {
+				app: 'group',
+				action: 'edit',
+				error: 'group-not-found'
+			} );
 		} else {
 			res.render( 'edit', { group: group } );
 		}
@@ -61,18 +90,44 @@ app.post( '/:id/edit', auth.isLoggedIn, function( req, res ) {
 	if ( req.body.name == '' ) {
 		req.flash( 'danger', 'The group requires a name' );
 		res.redirect( app.mountpath + '/edit' );
+
+		req.log.warn( {
+			app: 'group',
+			action: 'edit',
+			error: 'group-not-found'
+		} );
 	}
 
-	Groups.update( req.params.id, {
+	var group = {
 		name: req.body.name,
 		limiter: req.body.limiter ? req.body.limiter : null
-	}, function( err ) {
+	};
+
+	Groups.update( req.params.id, group, function( err ) {
 		if ( err ) {
 			req.flash( 'danger', 'There was an error updating the group' );
 			res.redirect( app.mountpath + '/' + req.params.id + '/edit' );
+
+			req.log.warn( {
+				app: 'groups',
+				action: 'edit',
+				error: 'error-updating-group',
+				sensitive: {
+					error: err
+				}
+			} );
 		} else {
 			req.flash( 'success', 'Group updated' );
 			res.redirect( app.mountpath );
+
+			req.log.debug( {
+				app: 'groups',
+				action: 'create',
+				message: 'Updated',
+				sensitive: {
+					group: group
+				}
+			} );
 		}
 	} );
 } )
@@ -98,6 +153,12 @@ app.get( '/:id/remove', auth.isLoggedIn, function( req, res ) {
 		} else {
 			req.flash( 'danger', 'Groups not found' );
 			res.redirect( app.mountpath );
+
+			req.log.warn( {
+				app: 'group',
+				action: 'remove',
+				error: 'group-not-found'
+			} );
 		}
 	} )
 } )
@@ -107,6 +168,12 @@ app.post( '/:id/remove', auth.isLoggedIn, function( req, res ) {
 		if ( ! group_to_remove ) {
 			req.flash( 'danger', 'Group not found' );
 			res.redirect( app.mountpath );
+
+			req.log.warn( {
+				app: 'group',
+				action: 'remove',
+				error: 'group-not-found'
+			} );
 			return;
 		}
 
@@ -115,6 +182,13 @@ app.post( '/:id/remove', auth.isLoggedIn, function( req, res ) {
 				if ( ! group_to_become ) {
 					req.flash( 'danger', 'New group not found' );
 					res.redirect( app.mountpath );
+
+					req.log.warn( {
+						app: 'group',
+						action: 'remove',
+						error: 'new-group-not-found'
+					} );
+
 					return;
 				}
 
@@ -122,6 +196,16 @@ app.post( '/:id/remove', auth.isLoggedIn, function( req, res ) {
 					if ( err ) {
 						req.flash( 'danger', 'Could not transfer items to new group' );
 						res.redirect( app.mountpath );
+
+						req.log.warn( {
+							app: 'group',
+							action: 'remove',
+							error: 'transfer-error',
+							sensitive: {
+								error: err
+							}
+						} );
+
 						return;
 					}
 
@@ -129,11 +213,27 @@ app.post( '/:id/remove', auth.isLoggedIn, function( req, res ) {
 						if ( err ) {
 							req.flash( 'danger', 'Could not remove group' );
 							res.redirect( app.mountpath );
+
+							req.log.warn( {
+								app: 'group',
+								action: 'remove',
+								error: 'remove-error',
+								sensitive: {
+									error: err
+								}
+							} );
+
 							return;
 						}
 
 						req.flash( 'success', 'Group deleted and items transferred' );
 						res.redirect( app.mountpath );
+
+						req.log.debug( {
+							app: 'group',
+							action: 'remove',
+							message: 'Removed and transfered'
+						} );
 					} );
 				} );
 			} );
