760c7392d093aebb7747ef5b57f0bb57fb1575bc
Tom Lynch <me@unknowndomain.co.uk>
2019-01-06 20:34:16 +0000

    Update the seeds for new mandatory barcode field

--- seeds/seed_locations.js
+++ seeds/seed_locations.js
@@ -5,12 +5,12 @@ exports.seed = function(knex, Promise) {
     .then(function () {
       // Inserts seed entries
       return knex('locations').insert([
-        { name: 'WG28a > Main shelves' },
-        { name: 'WG28a > Red trolley' },
-        { name: 'WG28a > Small cupboard' },
-        { name: 'WG28 > Teaching cupboard' },
-        { name: 'WG28 > VR cupboard' },
-        { name: 'WG28 > Electronics cupboard' }
+        { name: 'WG28a > Main shelves', barcode: 'WG28a.1' },
+        { name: 'WG28a > Red trolley', barcode: 'WG28a.2' },
+        { name: 'WG28a > Small cupboard', barcode: 'WG28a.3' },
+        { name: 'WG28 > Teaching cupboard', barcode: 'WG28.1' },
+        { name: 'WG28 > VR cupboard', barcode: 'WG28.2' },
+        { name: 'WG28 > Electronics cupboard', barcode: 'WG28.3' }
       ]);
     });
 };
