e94eb019eb15b6b31296ce4672f11fc817ccd7d7
Tom Lynch <me@unknowndomain.co.uk>
2019-04-07 00:48:39 +0100

    Database changes

---
+++ migrations/20190406131951_undo_rename_department.js
@@ -0,0 +1,20 @@
+
+exports.up = function(knex, Promise) {
+  return Promise.all([
+    knex.schema.table('items', table => {
+      table.dropForeign('department_id'); // This name is delibeartely wrong because Knex made a mess
+    }),
+    knex.raw('ALTER INDEX "public"."locations_barcode_unique" RENAME TO "departments_barcode_unique";'),
+    knex.schema.renameTable('locations', 'departments' )
+  ]);
+};
+
+exports.down = function(knex, Promise) {
+  return Promise.all([
+    knex.schema.renameTable('departments', 'locations' ),
+    knex.raw('ALTER INDEX "public"."departments_barcode_unique" RENAME TO "locations_barcode_unique";'),
+    knex.schema.table('items', table => {
+      table.foreign('location_id').references('locations.id');
+    })
+  ]);
+};
---
+++ migrations/20190406135613_move_location_date.js
@@ -0,0 +1,14 @@
+
+exports.up = function(knex, Promise) {
+  return Promise.resolve()
+    .then(() => knex('departments').select('id', 'name', 'barcode'))
+    .then((rows) => knex('locations').insert(rows))
+    .then(() => knex('departments').del())
+};
+
+exports.down = function(knex, Promise) {
+  return Promise.resolve()
+    .then(() => knex('locations').select('id', 'name', 'barcode'))
+    .then((rows) => knex('departments').insert(rows))
+    .then(() => knex('locations').del())
+};
