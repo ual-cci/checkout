40f760e0873e223621eb990b652e356b4e8cc43c
Tom Lynch <me@unknowndomain.co.uk>
2019-05-27 20:57:18 +0100

    Simplifying some code

--- src/js/template-locals.js
+++ src/js/template-locals.js
@@ -10,8 +10,9 @@ function templateLocals( req, res, next ) {
 	if ( process.env.NODE_ENV == "development" ) res.locals.dev = true;
 	res.locals.loggedInUser = req.user;
 	res.locals.moment = require( 'moment' );
-	res.locals.config = {};
-	res.locals.config.pw_tries = process.env.USER_PW_TRIES;
+	res.locals.config = {
+    pw_tries: process.env.USER_PW_TRIES
+  };
 	next();
 };
 
