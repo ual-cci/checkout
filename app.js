929bcc539af17fb9a2bb8d06c06eeadd722477fd
Tom Lynch <me@unknowndomain.co.uk>
2019-05-27 18:53:04 +0100

    Changes to express-flash and quickflash.js
    This moves the functionality of quickflash into a fork of express-flash and adds some other patches including one that prevents empty sessions.

--- app.js
+++ app.js
@@ -1,38 +1,36 @@
-require('dotenv').config();
+require('dotenv').config()
 
-const express = require('express');
-const http = require('http');
-const flash = require('express-flash');
-const bodyParser = require('body-parser');
-const helmet = require('helmet');
+const express = require('express')
+const http = require('http')
+const flash = require('express-flash')
+const bodyParser = require('body-parser')
+const helmet = require('helmet')
 
-const { auth } = require('./src/js/authentication.js');
-const sessions = require('./src/js/sessions.js');
-const appLoader = require('./src/js/app-loader.js');
-const quickFlash = require('./src/js/quickflash.js');
+const {auth} = require('./src/js/authentication.js')
+const sessions = require('./src/js/sessions.js')
+const appLoader = require('./src/js/app-loader.js')
 
-const app = express();
-const server = http.Server(app);
+const app = express()
+const server = http.Server(app)
 
-app.use(helmet());
-auth(app);
-app.use(express.static('./static'));
-sessions(app);
-app.use(flash());
-app.use(quickFlash);
+app.use(helmet())
+auth(app)
+app.use(express.static('./static'))
+sessions(app)
+app.use(flash())
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded( { extended: true }));
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
 
 // Use PUG to render pages
-app.set( 'views', './src/views' );
-app.set( 'view engine', 'pug' );
-app.set( 'view cache', false );
+app.set('views', './src/views')
+app.set('view engine', 'pug')
+app.set('view cache', false)
 
 // Load apps
-appLoader(app);
+appLoader(app)
 
 // Start server
 const listener = server.listen(process.env.APP_PORT ,process.env.APP_HOST, function () {
-  console.log('Server started');
-});
+  console.log('Server started')
+})
