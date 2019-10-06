7cf69a9a2bdda55afecd08d416b21ccbf8155478
Tom Lynch <me@unknowndomain.co.uk>
2019-10-06 03:13:10 +0100

    Reformatting apps/logout to new standard

--- apps/logout/controller.js
+++ apps/logout/controller.js
@@ -1,19 +1,19 @@
-const BaseController = require('../../src/js/common/BaseController.js');
+const BaseController = require('../../src/js/common/BaseController.js')
 
-const config = require('./config.json');
+const config = require('./config.json')
 
 class LogoutController extends BaseController {
-  constructor() {
-    super({ path: config.path });
-  }
+	constructor() {
+		super({path: config.path})
+	}
 
-  getRoot(req, res) {
-    req.logout();
-    req.session.save( function(err) {
-      if ( err ) throw new Error(err);
-      req.saveSessionAndRedirect('/login');
-    } )
-  }
+	getRoot(req, res) {
+		req.logout()
+		req.session.save(function(err) {
+			if (err) throw new Error(err)
+			req.saveSessionAndRedirect('/login')
+		})
+	}
 }
 
-module.exports = LogoutController;
+module.exports = LogoutController
