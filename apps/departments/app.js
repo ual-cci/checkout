bb3337ffe3c9f9fa9c4f75b0369881222013dd93
Tom Lynch <me@unknowndomain.co.uk>
2019-10-06 03:03:06 +0100

    Reformatting apps/departments to new standard

--- apps/departments/app.js
+++ apps/departments/app.js
@@ -1,43 +1,43 @@
-const express = require( 'express' );
+const express = require('express')
 
-const DepartmentController = require('./controller.js');
-const auth = require('../../src/js/authentication.js');
+const DepartmentController = require('./controller.js')
+const auth = require('../../src/js/authentication.js')
 
-const app = express();
+const app = express()
 
-app.set( 'views', __dirname + '/views' );
+app.set('views', __dirname + '/views')
 
 app.use((req, res, next) => {
-  req.controller = new DepartmentController();
-  next();
-});
+	req.controller = new DepartmentController()
+	next()
+})
 
 app.get('/', auth.currentUserCan('departments_read'), function(req, res) {
-  req.controller.getHome(req, res);
-});
+	req.controller.getHome(req, res)
+})
 
 app.get('/create', auth.currentUserCan('departments_create'), function(req, res) {
-  req.controller.getCreate(req, res);
-});
+	req.controller.getCreate(req, res)
+})
 
-app.post( '/create', auth.currentUserCan('departments_create'), function( req, res ) {
-  req.controller.postCreate(req, res);
-});
+app.post('/create', auth.currentUserCan('departments_create'), function(req, res) {
+	req.controller.postCreate(req, res)
+})
 
 app.get('/:id/edit', auth.currentUserCan('departments_edit'), function(req, res) {
-  req.controller.getEdit(req, res);
-});
+	req.controller.getEdit(req, res)
+})
 
 app.post('/:id/edit', auth.currentUserCan('departments_edit'), function(req, res) {
-  req.controller.postEdit(req, res);
-});
+	req.controller.postEdit(req, res)
+})
 
 app.get('/:id/remove', auth.currentUserCan('departments_remove'), function(req, res) {
-  req.controller.getRemove(req, res);
-});
+	req.controller.getRemove(req, res)
+})
 
 app.post('/:id/remove', auth.currentUserCan('departments_remove'), function(req, res) {
-  req.controller.postRemove(req, res);
-});
+	req.controller.postRemove(req, res)
+})
 
-module.exports = config => app;
+module.exports = config => app
