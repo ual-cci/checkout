32389905f90dfdda143cf0a6cd9cde7a19ceaf85
Tom Lynch <me@unknowndomain.co.uk>
2019-10-06 03:19:17 +0100

    Reformatting apps/profile to new standard

--- apps/profile/controller.js
+++ apps/profile/controller.js
@@ -1,81 +1,81 @@
-const BaseController = require('../../src/js/common/BaseController.js');
-const config = require('./config.json');
+const BaseController = require('../../src/js/common/BaseController.js')
+const config = require('./config.json')
 
-const auth = require('../../src/js/authentication.js');
+const auth = require('../../src/js/authentication.js')
 
-const Users = require('../../src/models/users.js');
-const Printers = require('../../src/models/printers.js');
+const Users = require('../../src/models/users.js')
+const Printers = require('../../src/models/printers.js')
 
 class ProfileController extends BaseController {
-  constructor() {
-    super({ path: config.path });
+	constructor() {
+		super({path: config.path})
 
-    this.models = {
-      users: new Users(),
-      printers: new Printers()
-    };
-  }
+		this.models = {
+			users: new Users(),
+			printers: new Printers()
+		}
+	}
 
-  /**
-   * Displays current users profile
-   *
-   * @param {Object} req Express request object
-   * @param {Object} res Express response object
-   */
-  getRoot(req, res) {
-      this.models.printers.getAll()
-        .then(printers => {
-          res.render('profile', {printers});
-        });
-  }
+	/**
+	* Displays current users profile
+	*
+	* @param {Object} req Express request object
+	* @param {Object} res Express response object
+	*/
+	getRoot(req, res) {
+		this.models.printers.getAll()
+		.then(printers => {
+			res.render('profile', {printers})
+		})
+	}
 
-  /**
-   * Posts an edit for current user
-   *
-   * @param {Object} req Express request object
-   * @param {Object} res Express response object
-   */
-  postRoot(req, res) {
-    var passwordValidation = auth.passwordRequirements(req.body.password);
-    if (req.body.password && passwordValidation !== true) {
-      req.flash('danger', passwordValidation);
-      req.saveSessionAndRedirect(this.getRoute());
-      return;
-    }
+	/**
+	* Posts an edit for current user
+	*
+	* @param {Object} req Express request object
+	* @param {Object} res Express response object
+	*/
+	postRoot(req, res) {
+		var passwordValidation = auth.passwordRequirements(req.body.password)
+		if (req.body.password && passwordValidation !== true) {
+			req.flash('danger', passwordValidation)
+			req.saveSessionAndRedirect(this.getRoute())
+			return
+		}
 
-    if (req.body.password && req.body.password != req.body.verify) {
-      req.flash('danger', 'Passwords must match');
-      req.saveSessionAndRedirect(this.getRoute());
-      return;
-    }
+		if (req.body.password && req.body.password != req.body.verify) {
+			req.flash('danger', 'Passwords must match')
+			req.saveSessionAndRedirect(this.getRoute())
+			return
+		}
 
-    const user = {
-      name: req.body.name,
-      email: req.body.email,
-      printer_id: req.body.printer ? req.body.printer : null,
-    };
+		const user = {
+			name: req.body.name,
+			email: req.body.email,
+			printer_id: req.body.printer ? req.body.printer : null,
+		}
 
-    if (req.body.audit_point) {
-      user.audit_point = new Date(req.body.audit_point);
-    } else {
-      user.audit_point = null;
-    }
+		if (req.body.audit_point) {
+			user.audit_point = new Date(req.body.audit_point)
+		} else {
+			user.audit_point = null
+		}
 
-    auth.generatePassword(req.body.password, password => {
-      if (req.body.password) {
-        user.pw_hash = password.hash;
-        user.pw_salt = password.salt;
-        user.pw_iterations = password.iterations;
-      }
+		auth.generatePassword(req.body.password, password => {
+			if (req.body.password) {
+				user.pw_hash = password.hash
+				user.pw_salt = password.salt
+				user.pw_iterations = password.iterations
+			}
 
-      this.models.users.update(req.user.id, user)
-        .then(id => {
-          req.flash('success', 'Profile updated');
-          req.saveSessionAndRedirect(this.getRoute());
-        })
-        .catch(err => this.displayError(req, res, err, this.getRoute()));
-    } );
-  }
-};
+			this.models.users.update(req.user.id, user)
+			.then(id => {
+				req.flash('success', 'Profile updated')
+				req.saveSessionAndRedirect(this.getRoute())
+			})
+			.catch(err => this.displayError(req, res, err, this.getRoute()))
+		})
+	}
+}
 
-module.exports = ProfileController;
+module.exports = ProfileController
