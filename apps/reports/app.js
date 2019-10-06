e75e7129f30db5400ada853cb8f5d0dc9166a062
Tom Lynch <me@unknowndomain.co.uk>
2019-10-06 03:20:01 +0100

    Reformatting apps/reports to new standard

--- apps/reports/app.js
+++ apps/reports/app.js
@@ -1,23 +1,23 @@
-const express = require( 'express' );
+const express = require('express')
 
-const ItemController = require('./controller');
-const auth = require('../../src/js/authentication');
+const ItemController = require('./controller')
+const auth = require('../../src/js/authentication')
 
-const app = express();
+const app = express()
 
-app.set( 'views', __dirname + '/views' );
+app.set('views', __dirname + '/views')
 
 app.use((req, res, next) => {
-  req.controller = new ItemController();
-  next();
-});
+	req.controller = new ItemController()
+	next()
+})
 
 app.get('/insurance', auth.currentUserCan('generate_insurance_report'), (req, res) => {
-  req.controller.getInsurance(req, res);
-});
+	req.controller.getInsurance(req, res)
+})
 
-app.post( '/insurance', auth.currentUserCan('generate_insurance_report'), (req, res) => {
-  req.controller.postInsurance(req, res);
-});
+app.post('/insurance', auth.currentUserCan('generate_insurance_report'), (req, res) => {
+	req.controller.postInsurance(req, res)
+})
 
-module.exports = config => app;
+module.exports = config => app
