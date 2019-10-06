5aeb4d8d98df65099641cfd630171aeba2a8d441
Tom Lynch <me@unknowndomain.co.uk>
2019-10-06 03:11:18 +0100

    Reformatting apps/kiosk to new standard

--- apps/kiosk/app.js
+++ apps/kiosk/app.js
@@ -1,39 +1,39 @@
-const express = require( 'express' );
+const express = require('express')
 
-const KioskController = require('./controller');
-const auth = require('../../src/js/authentication.js');
+const KioskController = require('./controller')
+const auth = require('../../src/js/authentication.js')
 
-const app = express();
+const app = express()
 
-app.set( 'views', __dirname + '/views' );
+app.set('views', __dirname + '/views')
 
 app.use((req, res, next) => {
-  req.controller = new KioskController();
-  next();
-});
+	req.controller = new KioskController()
+	next()
+})
 
 app.get('/', (req, res) => {
-  req.controller.getRoot(req, res);
-});
+	req.controller.getRoot(req, res)
+})
 
 app.post('/', (req, res) => {
-  req.controller.postRoot(req, res);
-});
+	req.controller.postRoot(req, res)
+})
 
 app.get('/enable', auth.currentUserCan('activate_kiosk_mode'), (req, res) => {
-  req.controller.getEnable(req, res);
-});
+	req.controller.getEnable(req, res)
+})
 
 app.post('/enable', auth.currentUserCan('activate_kiosk_mode'), (req, res) => {
-  req.controller.postEnable(req, res);
-});
+	req.controller.postEnable(req, res)
+})
 
 app.get('/logout', (req, res) => {
-  req.controller.getLogout(req, res);
-});
+	req.controller.getLogout(req, res)
+})
 
 app.get('/exit', (req, res) => {
-  req.controller.getExit(req, res);
-});
+	req.controller.getExit(req, res)
+})
 
-module.exports = config => app;
+module.exports = config => app
