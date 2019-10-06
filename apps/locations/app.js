b8097e0570d342f04be481bfecf3e9a9890d0c4e
Tom Lynch <me@unknowndomain.co.uk>
2019-10-06 03:12:16 +0100

    Reformatting apps/locations to new standard

--- apps/locations/app.js
+++ apps/locations/app.js
@@ -1,47 +1,47 @@
-const express = require( 'express' );
+const express = require('express')
 
-const LocationController = require('./controller.js');
-const auth = require('../../src/js/authentication.js');
+const LocationController = require('./controller.js')
+const auth = require('../../src/js/authentication.js')
 
-const app = express();
+const app = express()
 
-app.set( 'views', __dirname + '/views' );
+app.set('views', __dirname + '/views')
 
 app.use((req, res, next) => {
-  req.controller = new LocationController();
-  next();
-});
+	req.controller = new LocationController()
+	next()
+})
 
 app.get('/', auth.currentUserCan('locations_read'), (req, res) => {
-  req.controller.getRoot(req, res);
-});
+	req.controller.getRoot(req, res)
+})
 
 app.get('/create', auth.currentUserCan('locations_create'), (req, res) => {
-  req.controller.getCreate(req, res);
-});
+	req.controller.getCreate(req, res)
+})
 
-app.post( '/create', auth.currentUserCan('locations_create'), (req, res) => {
-  req.controller.postCreate(req, res);
-});
+app.post('/create', auth.currentUserCan('locations_create'), (req, res) => {
+	req.controller.postCreate(req, res)
+})
 
 app.get('/:id/edit', auth.currentUserCan('locations_create'), (req, res) => {
-  req.controller.getEdit(req, res);
-});
+	req.controller.getEdit(req, res)
+})
 
 app.post('/:id/edit', auth.currentUserCan('locations_edit'), (req, res) => {
-  req.controller.postEdit(req, res);
-});
+	req.controller.postEdit(req, res)
+})
 
 app.get('/:id/remove', auth.currentUserCan('locations_remove'), (req, res) => {
-  req.controller.getRemove(req, res);
-});
+	req.controller.getRemove(req, res)
+})
 
 app.post('/:id/remove', auth.currentUserCan('locations_remove'), (req, res) => {
-  req.controller.postRemove(req, res);
-});
+	req.controller.postRemove(req, res)
+})
 
-app.get( '/:id/label', auth.currentUserCan('print'), (req, res) => {
-  req.controller.getLabel(req, res);
-});
+app.get('/:id/label', auth.currentUserCan('print'), (req, res) => {
+	req.controller.getLabel(req, res)
+})
 
-module.exports = config => app;
+module.exports = config => app
--- apps/locations/controller.js
+++ apps/locations/controller.js
@@ -1,194 +1,194 @@
-const BaseController = require('../../src/js/common/BaseController.js');
+const BaseController = require('../../src/js/common/BaseController.js')
 
-const Locations = require('../../src/models/locations.js');
-const Items = require('../../src/models/items.js');
-const Printers = require('../../src/models/printers.js');
+const Locations = require('../../src/models/locations.js')
+const Items = require('../../src/models/items.js')
+const Printers = require('../../src/models/printers.js')
 
-const Print = require('../../src/js/print');
+const Print = require('../../src/js/print')
 
-const config = require('./config.json');
+const config = require('./config.json')
 
 class LocationController extends BaseController {
-  constructor() {
-    super({ path: config.path });
+	constructor() {
+		super({path: config.path})
 
-    this.models = {
-      locations: new Locations(),
-      items: new Items(),
-      printers: new Printers()
-    };
-  }
+		this.models = {
+			locations: new Locations(),
+			items: new Items(),
+			printers: new Printers()
+		}
+	}
 
-  getRoot(req, res) {
-    this.models.locations.getAll()
-      .then(locations => {
-        res.render('index', { locations });
-      });
-  }
+	getRoot(req, res) {
+		this.models.locations.getAll()
+		.then(locations => {
+			res.render('index', {locations})
+		})
+	}
 
-  getCreate(req, res) {
-    res.render('create', { location: {} });
-  }
+	getCreate(req, res) {
+		res.render('create', {location: {}})
+	}
 
-  postCreate(req, res) {
-    if (req.body.name == '') {
-      this.displayError(req, res, 'The location requires a name', this.getRoute('/create'), 'Error creating - ');
-    }
+	postCreate(req, res) {
+		if (req.body.name == '') {
+			this.displayError(req, res, 'The location requires a name', this.getRoute('/create'), 'Error creating - ')
+		}
 
-    var location = {
-      name: req.body.name
-    }
+		var location = {
+			name: req.body.name
+		}
 
-    if (req.body.barcode) {
-      location.barcode = req.body.barcode;
-    } else {
-      location.barcode = null;
-    }
+		if (req.body.barcode) {
+			location.barcode = req.body.barcode
+		} else {
+			location.barcode = null
+		}
 
-    this.models.locations.create(location)
-      .then(result => {
-        req.flash( 'success', 'Location created' );
-        req.saveSessionAndRedirect(this.mountPath);
-      })
-      .catch(err => this.displayError(req, res, err, this.getRoute('/create'), 'Location not created - '));
-  }
+		this.models.locations.create(location)
+		.then(result => {
+			req.flash('success', 'Location created')
+			req.saveSessionAndRedirect(this.mountPath)
+		})
+		.catch(err => this.displayError(req, res, err, this.getRoute('/create'), 'Location not created - '))
+	}
 
-  getEdit(req, res) {
-    this.models.locations.getById(req.params.id)
-      .then(location => {
-        if (!location) {
-          throw new Error('Location not found');
-        }
+	getEdit(req, res) {
+		this.models.locations.getById(req.params.id)
+		.then(location => {
+			if (!location) {
+				throw new Error('Location not found')
+			}
 
-        res.render('edit', { location });
-      })
-      .catch(err => this.displayError(req, res, err, this,getRoute()));
-  }
+			res.render('edit', {location})
+		})
+		.catch(err => this.displayError(req, res, err, this,getRoute()))
+	}
 
-  postEdit(req, res) {
-    if (req.body.name == '') {
-      this.displayError(req, res, 'The location requires a name', this.getRoute([`/${req.params.id}`, '/edit']), 'Error editing - ');
-    }
+	postEdit(req, res) {
+		if (req.body.name == '') {
+			this.displayError(req, res, 'The location requires a name', this.getRoute([`/${req.params.id}`, '/edit']), 'Error editing - ')
+		}
 
-    var location = {
-      name: req.body.name
-    }
+		var location = {
+			name: req.body.name
+		}
 
-    if (req.body.barcode) {
-      location.barcode = req.body.barcode;
-    } else {
-      location.barcode = null;
-    }
+		if (req.body.barcode) {
+			location.barcode = req.body.barcode
+		} else {
+			location.barcode = null
+		}
 
-    this.models.locations.update(req.params.id, location )
-      .then(() => {
-        req.flash( 'success', 'Location updated' );
-        req.saveSessionAndRedirect(this.mountPath);
-      })
-      .catch(err => this.displayError(req, res, err, this.getRoute([`/${req.params.id}`, '/edit']), 'Location not updated - '));
-  }
+		this.models.locations.update(req.params.id, location)
+		.then(() => {
+			req.flash('success', 'Location updated')
+			req.saveSessionAndRedirect(this.mountPath)
+		})
+		.catch(err => this.displayError(req, res, err, this.getRoute([`/${req.params.id}`, '/edit']), 'Location not updated - '))
+	}
 
-  getRemove(req, res) {
-    this.models.locations.getAll()
-      .then(locations => {
-        const selected = locations.find(d => d.id === parseInt(req.params.id, 10));
+	getRemove(req, res) {
+		this.models.locations.getAll()
+		.then(locations => {
+			const selected = locations.find(d => d.id === parseInt(req.params.id, 10))
 
-        if (!selected) {
-          throw new Error('locations not found');
-        }
+			if (!selected) {
+				throw new Error('locations not found')
+			}
 
-        const list = locations.map(location => {
-          if (location.id == req.params.id) {
-            return Object.assign({}, location, {
-              disabled: true
-            });
-          }
+			const list = locations.map(location => {
+				if (location.id == req.params.id) {
+					return Object.assign({}, location, {
+						disabled: true
+					})
+				}
 
-          return location;
-        });
+				return location
+			})
 
-        res.render('confirm-remove', {
-          selected: selected,
-          locations: list
-        });
-      })
-      .catch(err => this.displayError(req, res, err, this.getRoute()));
-  }
+			res.render('confirm-remove', {
+				selected: selected,
+				locations: list
+			})
+		})
+		.catch(err => this.displayError(req, res, err, this.getRoute()))
+	}
 
-  /**
-   * Endpoint for removing a location and transferring
-   * all items to a new location
-   *
-   * @param {Object} req Express request object
-   * @param {Object} res Express response object
-   */
-  postRemove(req, res) {
-    let removeId;
+	/**
+	 * Endpoint for removing a location and transferring
+	 * all items to a new location
+	 *
+	 * @param {Object} req Express request object
+	 * @param {Object} res Express response object
+	 */
+	postRemove(req, res) {
+		let removeId
 
-    Promise.all([
-      this.models.locations.query().getById(req.params.id),
-      this.models.locations.query().getById(req.body.location)
-    ])
-      .then(([ locationToRemove, locationToBecome ]) => {
-        if (!locationToBecome || !locationToRemove) {
-          throw new Error('Location to remove/become not found');
-        }
+		Promise.all([
+			this.models.locations.query().getById(req.params.id),
+			this.models.locations.query().getById(req.body.location)
+		])
+		.then(([ locationToRemove, locationToBecome ]) => {
+			if (!locationToBecome || !locationToRemove) {
+				throw new Error('Location to remove/become not found')
+			}
 
-        removeId = locationToRemove.id;
-        return { locationToRemove, locationToBecome };
-      })
-      .then(({ locationToRemove, locationToBecome }) => {
-        return this.models.items.updateLocation(locationToRemove.id, locationToBecome.id);
-      })
-      .then(() => {
-        return this.models.locations.remove(removeId);
-      })
-      .then(() => {
-        req.flash( 'success', 'Location deleted and items transferred' );
-        req.saveSessionAndRedirect(this.getRoute());
-      })
-      .catch(err => {
-        this.displayError(req, res, err, this.getRoute(), 'Error removing post - ');
-      });
-  }
+			removeId = locationToRemove.id
+			return {locationToRemove, locationToBecome}
+		})
+		.then(({locationToRemove, locationToBecome}) => {
+			return this.models.items.updateLocation(locationToRemove.id, locationToBecome.id)
+		})
+		.then(() => {
+			return this.models.locations.remove(removeId)
+		})
+		.then(() => {
+			req.flash('success', 'Location deleted and items transferred')
+			req.saveSessionAndRedirect(this.getRoute())
+		})
+		.catch(err => {
+			this.displayError(req, res, err, this.getRoute(), 'Error removing post - ')
+		})
+	}
 
 
-  getLabel(req, res) {
-    let persist = {};
+	getLabel(req, res) {
+		let persist = {}
 
-    this.models.locations.getById(req.params.id)
-      .then(location => {
-        if (!location) {
-          throw new Error('Location not found');
-        }
+		this.models.locations.getById(req.params.id)
+		.then(location => {
+			if (!location) {
+				throw new Error('Location not found')
+			}
 
-        persist.location = location;
-        persist.printerId = req.query.printer || req.user.printer_id;
+			persist.location = location
+			persist.printerId = req.query.printer || req.user.printer_id
 
-        if (!persist.printerId) {
-          throw new Error('No printer selected');
-        }
+			if (!persist.printerId) {
+				throw new Error('No printer selected')
+			}
 
-        return this.models.printers.getById(persist.printerId);
-      })
-      .then(printer => {
-        const { location } = persist;
+			return this.models.printers.getById(persist.printerId)
+		})
+		.then(printer => {
+			const {location} = persist
 
-        if (!printer) {
-          throw new Error('Invalid printer');
-        }
+			if (!printer) {
+				throw new Error('Invalid printer')
+			}
 
-        Print.label( {
-          barcode: 'L:' + location.barcode,
-          text: location.barcode,
-          type: '36mm'
-        }, printer.url );
+			Print.label({
+				barcode: 'L:' + location.barcode,
+				text: location.barcode,
+				type: '36mm'
+			}, printer.url)
 
-        req.flash('info', `Label printed to ${printer.name}`);
-        req.saveSessionAndRedirect(this.getRoute());
-      })
-      .catch(err => this.displayError(req, res, err, this.getRoute()));
-  }
+			req.flash('info', `Label printed to ${printer.name}`)
+			req.saveSessionAndRedirect(this.getRoute())
+		})
+		.catch(err => this.displayError(req, res, err, this.getRoute()))
+	}
 }
 
-module.exports = LocationController;
+module.exports = LocationController
