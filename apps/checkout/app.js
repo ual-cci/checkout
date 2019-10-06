60d8021447e7eec896e408c5838a6a6e94dfcd4b
Tom Lynch <me@unknowndomain.co.uk>
2019-10-06 02:59:10 +0100

    Reformatting apps/checkout to new standard

--- apps/checkout/app.js
+++ apps/checkout/app.js
@@ -1,19 +1,19 @@
-const express = require( 'express' );
+const express = require('express')
 
-const auth = require('../../src/js/authentication.js');
-const CheckoutController = require('./controller.js');
+const auth = require('../../src/js/authentication.js')
+const CheckoutController = require('./controller.js')
 
-const app = express();
+const app = express()
 
-app.set( 'views', __dirname + '/views' );
+app.set('views', __dirname + '/views')
 
 app.use((req, res, next) => {
-  req.controller = new CheckoutController();
-  next();
-});
+	req.controller = new CheckoutController()
+	next()
+})
 
-app.get( '/', auth.currentUserCan({or:['checkout_issue','checkout_return','checkout_audit','checkout_history','print','users_create','reservations_create']}), (req, res) => {
-  req.controller.getRoot(req, res);
-});
+app.get('/', auth.currentUserCan({or:['checkout_issue','checkout_return','checkout_audit','checkout_history','print','users_create','reservations_create']}), (req, res) => {
+	req.controller.getRoot(req, res)
+})
 
-module.exports = config => app;
+module.exports = config => app
