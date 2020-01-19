37743b8facaebc28a4f3252323bc6e228b1276f2
Tom Lynch <me@unknowndomain.co.uk>
2020-01-19 11:24:59 +0000

    Simplify home redirect and check for kioskMode

--- apps/home/app.js
+++ apps/home/app.js
@@ -6,18 +6,16 @@ const app = express()
 app.set('views', __dirname + '/views')
 
 app.get('/', auth.isLoggedIn, (req, res) => {
-	if (req.user.role_home) {
-		if (req.user.role_home == '/checkout') {
-			if (auth.userCan(req.user, 'checkout_issue',{or:['checkout_return','print','checkout_audit','checkout_history','checkout_new_user','checkout_new_reservation']})) {
-				req.saveSessionAndRedirect('/checkout')
-			} else {
-				res.render('index')
-			}
-		} else {
-			req.saveSessionAndRedirect(req.user.role_home)
-		}
+	// Always go to checkout if in Kiosk mode
+	if (req.session.kioskMode > 0) {
+		req.saveSessionAndRedirect('/checkout')
 	} else {
-		res.render('index')
+		// Check user has a role home, they should, but ...
+		if (req.user.role_home) {
+			req.saveSessionAndRedirect(req.user.role_home)
+		} else {
+			res.render('index')
+		}
 	}
 })
 
