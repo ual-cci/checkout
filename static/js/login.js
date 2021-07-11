c3dde5c223c528d522ece539ed9be8fe40fff324
Tom Lynch <me@unknowndomain.co.uk>
2021-07-11 17:21:19 +0100

    Redundant js file

--- static/js/login.js
+++
@@ -1,9 +0,0 @@
-jQuery(document).ready(function() {
-	$('#id').focus()
-
-	$('#login').submit(function(e) {
-		e.preventDefault()
-		if ($('#id').val() != '')
-			io.emit('login', $('#id').val())
-	})
-})
