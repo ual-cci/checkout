16e6d2d24d047a55efb0904703aa76936541e1c9
Tom Lynch <me@unknowndomain.co.uk>
2021-07-18 21:09:43 +0100

    Add quicksearch keyboard shortcut

--- static/js/quicksearch.js
+++ static/js/quicksearch.js
@@ -2,11 +2,21 @@ let searchTextField, checkAllBox
 
 window.addEventListener('load', () => {
 	searchTextField = document.querySelector('#searchText input')
-	if (searchTextField) searchTextField.addEventListener('input', quickSearch)
+	if (searchTextField) {
+		searchTextField.addEventListener('input', quickSearch)
+		document.addEventListener('keydown', QShandleKeyboardInput)
+	}
 
 	checkAllBox = document.querySelector('input[type=checkbox].checkall')
 	checkAllBox.addEventListener('click', checkAll)
 })
+
+function QShandleKeyboardInput(e) {
+	if (e.altKey && e.shiftKey && e.which == 81) {
+		e.preventDefault()
+		searchTextField.focus()
+	}
+}
 
 function checkAll() {
 	if (checkAllBox.checked) {
