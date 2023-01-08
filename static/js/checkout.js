21621904bac8fa556a6e4032b7fc16d94ca4f2e4
Tom Lynch <t.lynch@arts.ac.uk>
2023-01-08 02:08:07 +0000

    Move apiPOST back

--- static/js/checkout.js
+++ static/js/checkout.js
@@ -508,3 +508,31 @@ function lazyResetKioskTimer() {
 		resetKioskTimer()
 	}
 }
+
+function apiPOST(path, data, cb) {
+	if (typeof data == 'function') {
+		cb = data
+		delete data
+	}
+
+	let request = {
+		url: `/api/${path}`,
+		type: 'post',
+		headers: {
+			'CSRF-Token': token
+		},
+		xhrFields: {
+			withCredentials: true
+		},
+		dataType: 'json',
+		success: (data, status) => {
+			cb(data)
+		}
+	}
+
+	if (typeof data == 'object') {
+		request.data = data
+	}
+
+	jQuery.ajax(request)
+}
