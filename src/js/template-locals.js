37efe51fddc5e289f120522c84a4dd115104efc0
Tom Lynch <t.lynch@arts.ac.uk>
2023-01-08 15:18:23 +0000

    Fix printer and template not loading on all pages

--- src/js/template-locals.js
+++ src/js/template-locals.js
@@ -2,6 +2,7 @@ const auth = require('./authentication')
 const Options = require('./options')()
 const Printers = require('../models/printers')
 const Templates = require('../models/templates')
+const moment = require('moment')
 
 
 const printers = new Printers()
@@ -15,27 +16,30 @@ gitRev.short(str => {
 })
 
 function templateLocals(req, res, next) {
-	printers.getAll().then((printers) => {
+	Promise.all([
+		printers.getAll(),
+		templates.getAll()
+	])
+	.then(([printers, templates]) => {
 		res.locals.printers = printers
-	})
-	templates.getAll().then((templates) => {
 		res.locals.templates = templates
-	})
 
-	res.locals.breadcrumb = []
-	res.locals.git = git
-	if (process.env.NODE_ENV == "development") res.locals.dev = true
-	if (req.session.kioskMode > 0) res.locals.kioskMode = true
-	if (req.csrfToken) res.locals.csrf = req.csrfToken()
-	res.locals.loggedInUser = req.user
-	res.locals.currentUserCan = (perm) => {
-		return auth.userCan(req.user,perm)
-	}
-	res.locals.Options = {}
-	res.locals.Options.get = Options.getText
-	res.locals.Options.getBoolean = Options.getBoolean
-	res.locals.moment = require('moment')
-	next()
+		res.locals.breadcrumb = []
+		res.locals.git = git
+		if (process.env.NODE_ENV == "development") res.locals.dev = true
+		if (req.session.kioskMode > 0) res.locals.kioskMode = true
+		if (req.csrfToken) res.locals.csrf = req.csrfToken()
+		res.locals.loggedInUser = req.user
+		res.locals.currentUserCan = (perm) => {
+			return auth.userCan(req.user,perm)
+		}
+		res.locals.Options = {}
+		res.locals.Options.get = Options.getText
+		res.locals.Options.getBoolean = Options.getBoolean
+		res.locals.moment = moment
+
+		next()
+	})
 }
 
 module.exports = () => {
