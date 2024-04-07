e5b1cef0d6af5b9354babba507ef217ed193ecb0
Tom Lynch <t.lynch@arts.ac.uk>
2024-04-08 00:50:16 +0100

    Remove redundant tempusdominus

--- app.js
+++ app.js
@@ -24,7 +24,6 @@ app.use('/clipboard', express.static('./node_modules/clipboard/dist'))
 app.use('/jquery', express.static('./node_modules/jquery/dist'))
 app.use('/fontawesome', express.static('./node_modules/@fortawesome/fontawesome-free'))
 app.use('/moment', express.static('./node_modules/moment/min'))
-app.use('/tempusdominus', express.static('./node_modules/tempusdominus-bootstrap-4/build'))
 
 sessions(app)
 auth(app)
