98f3d1353311c75a96710c3abde73948a471a6bb
Tom Lynch <t.lynch@arts.ac.uk>
2024-04-08 01:22:11 +0100

    Rewrite password-requirements.js to not require jQuery

--- static/js/password-requirements.js
+++ static/js/password-requirements.js
@@ -1,52 +1,74 @@
-jQuery(document).ready(function($) {
-	var options = {
-		trigger: 'focus',
-		title: 'Password requirements',
-		content: '<p class="pw len">8 characters or more</p><p class="pw up">1 or more uppercase letters</p><p class="pw low">1 or more lowercase letters</p><p class="pw num">1 or more numbers</p><p class="pw special">1 or more special characters</p><p class="pw mat">Passwords match</p>',
-		html: true,
-		placement: 'top'
-	}
-	jQuery('.pw-req').popover(options).on('keyup paste focus', check)
-	jQuery('.pw-mat').popover(options).on('keyup paste focus', check)
+let pw
+let mat
+
+const options = {
+	trigger: 'focus',
+	html: true,
+	title: 'Requirements',
+	content: '<p class="pw len">8 characters</p><p class="pw up">Uppercase letters</p><p class="pw low">Lowercase letters</p><p class="pw num">Numbers</p><p class="pw special">Special characters</p><p class="pw mat">Passwords match</p>',
+}
+
+window.addEventListener('DOMContentLoaded', () => {
+	pw  = document.querySelector('.pw-req')
+	mat = document.querySelector('.pw-mat')
+
+	let pw_popover = new bootstrap.Popover(pw, options)
+	pw_popover._element.addEventListener('inserted.bs.popover', check)
+	pw_popover._element.addEventListener('shown.bs.popover', check)
+	pw.addEventListener('input', check)
+
+	let mat_popover = new bootstrap.Popover(mat, options)
+	mat_popover._element.addEventListener('inserted.bs.popover', check)
+	mat_popover._element.addEventListener('shown.bs.popover', check)
+	mat.addEventListener('input', check)
 })
 
 function check() {
-	var pw = jQuery('.pw-req').val()
-	var mat = jQuery('.pw-mat').val()
-
-	if (pw.length < 8) {
-		jQuery('.pw.len').removeClass('pass')
+	if (pw.value.length < 8) {
+		document.querySelector('.pw.len').classList.remove('pass')
+		document.querySelector('.pw.len').classList.add('fail')
 	} else {
-		jQuery('.pw.len').addClass('pass')
+		document.querySelector('.pw.len').classList.add('pass')
+		document.querySelector('.pw.len').classList.remove('fail')
 	}
 
-	if (pw.match(/\d/g) == null) {
-		jQuery('.pw.num').removeClass('pass')
+	if (pw.value.match(/\d/g) == null) {
+		document.querySelector('.pw.num').classList.remove('pass')
+		document.querySelector('.pw.num').classList.add('fail')
 	} else {
-		jQuery('.pw.num').addClass('pass')
+		document.querySelector('.pw.num').classList.add('pass')
+		document.querySelector('.pw.num').classList.remove('fail')
 	}
 
-	if (pw.match(/[A-Z]/g) == null) {
-		jQuery('.pw.up').removeClass('pass')
+	if (pw.value.match(/[A-Z]/g) == null) {
+		document.querySelector('.pw.up').classList.remove('pass')
+		document.querySelector('.pw.up').classList.add('fail')
 	} else {
-		jQuery('.pw.up').addClass('pass')
+		document.querySelector('.pw.up').classList.add('pass')
+		document.querySelector('.pw.up').classList.remove('fail')
 	}
 
-	if (pw.match(/[a-z]/g) == null) {
-		jQuery('.pw.low').removeClass('pass')
+	if (pw.value.match(/[a-z]/g) == null) {
+		document.querySelector('.pw.low').classList.remove('pass')
+		document.querySelector('.pw.low').classList.add('fail')
 	} else {
-		jQuery('.pw.low').addClass('pass')
+		document.querySelector('.pw.low').classList.add('pass')
+		document.querySelector('.pw.low').classList.remove('fail')
 	}
 
-	if (pw.match(/[^A-z0-9]/g) == null) {
-		jQuery('.pw.special').removeClass('pass')
+	if (pw.value.match(/[^A-z0-9]/g) == null) {
+		document.querySelector('.pw.special').classList.remove('pass')
+		document.querySelector('.pw.special').classList.add('fail')
 	} else {
-		jQuery('.pw.special').addClass('pass')
+		document.querySelector('.pw.special').classList.add('pass')
+		document.querySelector('.pw.special').classList.remove('fail')
 	}
 
-	if (pw == mat && pw.trim() != '') {
-		jQuery('.pw.mat').addClass('pass')
+	if (pw.value == mat.value && pw.value.trim() != '') {
+		document.querySelector('.pw.mat').classList.add('pass')
+		document.querySelector('.pw.mat').classList.remove('fail')
 	} else {
-		jQuery('.pw.mat').removeClass('pass')
+		document.querySelector('.pw.mat').classList.remove('pass')
+		document.querySelector('.pw.mat').classList.add('fail')
 	}
 }
